c094c819289ee240f131573acabc1b7c4f5b45c1
Sandra <gran.sl@gmail.com>
2024-11-25 13:21:24 -0800

    Added Javadoc, updated preconditions for setName, setStatus, addFriend. Changed constructor to make use of the preconditions for these methods.

--- src/main/java/Profile.java
+++ src/main/java/Profile.java
@@ -5,47 +5,99 @@ import java.util.ArrayList;
  * holding information about the user
  */
 public class Profile {
+    /** Name of the profile user */
     private String name;
+    /** File Name for the profile picture */
     private String picture;
+    /** Status of the User, can be any String, but usually Active/Inactive.*/
     private String status;
+    /** List of profile user's friends*/
     private ArrayList<String> friendsList;
 
+    /**
+     * Full Constructor
+     * @param name name of the Profile, cannot be empty or null.
+     * @param picture filename for the picture
+     * @param status status string, cannot be empty or null.
+     */
     public Profile(String name, String picture, String status) {
-        this.name = name;
+        setName(name);
         this.picture = picture;
-        this.status = status;
+        setStatus(status);
         friendsList = new ArrayList<>();
     }
 
     // GETTER METHODS
 
+    /**
+     * Returns the name associated with the Profile
+     *
+     * @return name of the profile
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the file name of the picture associated with the Profile.
+     *
+     * @return file name of the picture
+     */
     public String getPicture() {
         return picture;
     }
 
+    /**
+     * Returns the current status of the profile
+     *
+     * @return status of profile
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Returns the list of friends associated with this Profile.
+     *
+     * @return list of friends
+     */
     public ArrayList<String> getFriendsList() {
         return friendsList;
     }
 
     // SETTER METHODS
 
+    /**
+     * Sets a new name to be associated with the Profile.
+     *
+     * @param name new name for Profile, must not be empty or null
+     */
     public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name must not be empty or null");
+        }
         this.name = name;
     }
 
+    /**
+     * Sets a new picture file name to be associated with the Profile. This can be left empty
+     * as profile picture is optional.
+     *
+     * @param picture new file name for picture, can be empty
+     */
     public void setPicture(String picture) {
         this.picture = picture;
     }
 
+    /**
+     * Sets a new current status for the Profile.
+     *
+     * @param status new status, must not be empty or null
+     */
     public void setStatus(String status) {
+        if (status == null || status.isEmpty()) {
+            throw new IllegalArgumentException("Name must not be empty or null");
+        }
         this.status = status;
     }
 
@@ -54,7 +106,7 @@ public class Profile {
      * @param friend a String referencing another profiles name
      */
     public void addFriend(String friend) {
-        if(!friendsList.contains(friend) && friend != null) {
+        if(!friendsList.contains(friend) && friend != null && !friend.isEmpty()) {
             friendsList.add(friend);
         }
     }
@@ -70,7 +122,7 @@ public class Profile {
     }
 
     /**
-     * toSting method
+     * toString method
      * @return String containing profile information
      */
     @Override
