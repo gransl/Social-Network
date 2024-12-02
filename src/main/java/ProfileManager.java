1a04e56a4e4ce6629b5e5d872b43635877f9bfe5
user.email <skyeguy2003@gmail.com>
2024-12-02 10:07:21 -0800

    Updates to Profile and ProfileManager commit

--- src/main/java/ProfileManager.java
+++ src/main/java/ProfileManager.java
@@ -7,23 +7,35 @@ import java.util.Iterator;
 import java.util.Queue;
 
 public class ProfileManager {
+    /** Unsorted Linked Dictionary storing profiles
+     * with the key being the profiles username */
     private UnsortedLinkedDictionary<String, Profile> users;
+    /** Undirected Graph storing usernames of profiles */
     private UndirectedGraph<String> socialNetwork;
 
+    /**
+     * Full Constructor
+     */
     public ProfileManager() {
         users = new UnsortedLinkedDictionary<>();
         socialNetwork = new UndirectedGraph<>();
     }
 
+    /**
+     * Populates the social network with seven different profiles
+     * and connect them with each other as friends as seen in the UML diagram
+     */
     public void populateInitialGraph() {
-        Profile mustard = new Profile("Mustard", "mustard.png", "Active");
-        Profile plum = new Profile("Plum", "plum.png", "Active");
-        Profile peacock = new Profile("Peacock", "peacock.png", "Active");
-        Profile green = new Profile("Green", "green.png", "Inactive");
-        Profile white = new Profile("White", "white.png", "Active");
-        Profile scarlet = new Profile("Scarlet", "scarlet.png", "Active");
-        Profile boddy = new Profile("Boddy", "boddy.png", "Active");
+        // Create the seven different profiles
+        Profile mustard = new Profile("mustard", "Mustard", "mustard.png", "Active");
+        Profile plum = new Profile("plum", "Plum", "plum.png", "Active");
+        Profile peacock = new Profile("peacock", "Peacock", "peacock.png", "Active");
+        Profile green = new Profile("green", "Green", "green.png", "Inactive");
+        Profile white = new Profile("white", "White", "white.png", "Active");
+        Profile scarlet = new Profile("scarlet", "Scarlet", "scarlet.png", "Active");
+        Profile boddy = new Profile("boddy", "Boddy", "boddy.png", "Active");
 
+        // Add the profiles to the social network
         addProfile(mustard);
         addProfile(plum);
         addProfile(peacock);
@@ -32,48 +44,80 @@ public class ProfileManager {
         addProfile(scarlet);
         addProfile(boddy);
 
-        createFriendship(mustard.getName(), plum.getName());
-        createFriendship(mustard.getName(), green.getName());
-        createFriendship(mustard.getName(), scarlet.getName());
+        // Connect the profiles as friends
+        createFriendship(mustard.getUsername(), plum.getUsername());
+        createFriendship(mustard.getUsername(), green.getUsername());
+        createFriendship(mustard.getUsername(), scarlet.getUsername());
 
-        createFriendship(plum.getName(), green.getName());
+        createFriendship(plum.getUsername(), green.getUsername());
 
-        createFriendship(green.getName(), white.getName());
+        createFriendship(green.getUsername(), white.getUsername());
 
-        createFriendship(scarlet.getName(), peacock.getName());
+        createFriendship(scarlet.getUsername(), peacock.getUsername());
     }
 
+    /**
+     * Adds profile to the social network
+     *
+     * @return boolean true if the profile has been added
+     * and false if the profile has already been added
+     */
     public boolean addProfile(Profile profile) {
-        if(!users.contains(profile.getName())) {
-            socialNetwork.addVertex(profile.getName());
-            users.add(profile.getName(), profile);
-            return true;
+
+        // Check if the given username already exists in the social network
+        if(!users.contains(profile.getUsername())) {
+
+            // Add the username to the socialNetwork
+            socialNetwork.addVertex(profile.getUsername());
+            // Add the username as a key to the users and the given profile as a value
+            users.add(profile.getUsername(), profile);
+            return true; // Return True because the profile has been added
         }
-        return false;
+
+        return false; // Return false because the profile wasn't added
     }
 
-    public boolean removeProfile(String name) {
-        if(users.contains(name)) {
-            Profile profileToRemove = users.getValue(name);
+    /**
+     * Removes a profile from the social network
+     * @param username a username of a profile to remove
+     * @return boolean true if the user has been remove
+     * false if the given username doesn't exist in the social network
+     */
+    public boolean removeProfile(String username) {
+
+        // Check if users contains the given username
+        if(users.contains(username)) {
+
+            // Create a profile to remove getting the profile value from users with the username as the key
+            Profile profileToRemove = users.getValue(username);
+            // Get the profiles list of friends
             ArrayList<String> friendsList = profileToRemove.getFriendsList();
 
+            // Loop through each of the profile friends
             for (String friend : friendsList) {
+                // Create a profile of the friend
                 Profile friendProfile = users.getValue(friend);
-                friendProfile.getFriendsList().remove(name);
+                // Remove the given username from the list of friends of the friend profile
+                friendProfile.getFriendsList().remove(username);
             }
 
-            users.remove(name);
-            socialNetwork.removeVertex(name);
+            // Remove the username from users and socialNetwork
+            // Return true because the profile has been removed
+            users.remove(username);
+            socialNetwork.removeVertex(username);
             return true;
         }
+
+        // Return false because the given username doesn't exist in the social network
+        // and cannot be removed
         return false;
     }
 
-    public boolean createFriendship(String name1, String name2) {
-        if(users.contains(name1) && users.contains(name2)) {
-            socialNetwork.addEdge(name1, name2);
-            users.getValue(name1).addFriend(name2);
-            users.getValue(name2).addFriend(name1);
+    public boolean createFriendship(String username1, String username2) {
+        if(users.contains(username1) && users.contains(username2)) {
+            socialNetwork.addEdge(username1, username2);
+            users.getValue(username1).addFriend(username2);
+            users.getValue(username2).addFriend(username1);
             return true;
         }
         return false;
@@ -93,8 +137,8 @@ public class ProfileManager {
 
     }
 
-    public void displayCurrentUsersFriends(String name) {
-        Profile profile = users.getValue(name);
+    public void displayCurrentUsersFriends(String username) {
+        Profile profile = users.getValue(username);
         ArrayList<String> friends = profile.getFriendsList();
 
         if (!friends.isEmpty()) {
@@ -102,29 +146,39 @@ public class ProfileManager {
                 if (i > 0) {
                     System.out.print(", ");
                 }
-                System.out.print(friends.get(i));
+                System.out.print(users.getValue(friends.get(i)).getName());
             }
         } else {
             System.out.println("No friends");
         }
     }
 
-    public void displayCurrentUsersFriendsFriends(String name) {
-        Profile userProfile = users.getValue(name);
+    public void displayCurrentUsersFriendsFriends(String username) {
+        Profile userProfile = users.getValue(username);
         ArrayList<String> userFriends = userProfile.getFriendsList();
-
+        boolean firstFriend = true;
 
         for (String friend : userFriends) {
             Profile friendProfile = users.getValue(friend);
             ArrayList<String> friendsFriends = friendProfile.getFriendsList();
+            Iterator<String> friendsIterator = friendsFriends.iterator();
+            StringBuilder friendsFriendsList = new StringBuilder();
 
-            System.out.println(friend + " Friends:");
-            for (int i = 1; i < friendsFriends.size(); i++) {
-                if (i > 1) {
-                    System.out.print(", ");
+            System.out.println(friendProfile.getName() + " Friends:");
+
+            while (friendsIterator.hasNext()) {
+                String name = friendsIterator.next();
+                if (!name.equals(username)) {
+
+                    if (!friendsFriendsList.isEmpty()) {
+                        friendsFriendsList.append(", ");
+                    }
+                    friendsFriendsList.append(name);
                 }
-                System.out.print(friendsFriends.get(i));
             }
+
+
+            System.out.println(friendsFriendsList);
             System.out.println();
             System.out.println();
         }
@@ -143,7 +197,7 @@ public class ProfileManager {
                 if (i > 0) {
                     System.out.print(", ");
                 }
-                System.out.print(friends.get(i));
+                System.out.print((users.getValue(friends.get(i)).getName()));
             }
             System.out.println();
             System.out.println();
@@ -170,5 +224,24 @@ public class ProfileManager {
             allNamesList.add(keyIterator.next());
         }
         return allNamesList;
+    }
+
+    public void availableFriends(String username) {
+        ArrayList<String> friendsList = users.getValue(username).getFriendsList();
+        Iterator<String> keyIterator = users.getKeyIterator();
+        StringBuilder availableFriends = new StringBuilder();
+
+        while (keyIterator.hasNext()) {
+            String name = keyIterator.next();
+            if (!name.equals(username) && !friendsList.contains(name)) {
+
+                if (!availableFriends.isEmpty()) {
+                    availableFriends.append(", ");
+                }
+                availableFriends.append(name);
+            }
+        }
+
+        System.out.println(availableFriends);
     }
 }
